7b672a85fa87387c1c600944ecaa0123de5134fa
seven1H <lodaccx@gmail.com>
2023-08-15 18:46:29 +0800

    overview

--- src/basicsyntax/overview/overview_test.go
+++ src/basicsyntax/overview/overview_test.go
@@ -429,42 +429,43 @@ func TestCvAndCk(t *testing.T) {
 
 }
 
-/*
-string:
-
-	data struct string mem struct is like this:
-
-	 +-----------+          +---+---+---+---+---+
-	 |  pointer -|--------> | h | e | l | l | o |
-	 +-----------+          +---+---+---+---+---+
-	 |  len = 5  |
-	 +-----------+          [...]byte, UTF-8
-		header
-
-		// runtime/string.go
-		type stringStruct struct {
-			str unsafe.Pointer     // str *int   8 byte
-			len int
-		}
-
-		type stringStructDWARF struct {
-			str *byte				// str *uint8  1byte
-			len int
-		}
-
-		int  	字节大小:8  最小值:-9223372036854775808  最大值:9223372036854775807  2^(64-1)-1:  	64位有符号整数的数据范围: -9223372036854775808 到 9223372036854775807
-		uint8  	字节大小:1  最小值:0  					  最大值:255  				 2^8:  			8位无符号整数的数据范围: 0 到 255
-
-		编码 UTF-8，无 NULL 结尾，默认值 ""。
-		使用 `raw string` 定义原始字符串。
-		支持 !=、==、<、<=、>=、>、+、+=。
-		索引访问字节数组（非字符），不能获取元素地址。
-		切片返回子串，依旧指向原数组。
-		内置函数 len 返回字节数组长度。
-
--
-*/
 func TestString(t *testing.T) {
+	/*
+		string:
+
+			data struct string mem struct is like this:
+
+			 +-----------+          +---+---+---+---+---+
+			 |  pointer -|--------> | h | e | l | l | o |
+			 +-----------+          +---+---+---+---+---+
+			 |  len = 5  |
+			 +-----------+          [...]byte, UTF-8
+				header
+
+				// runtime/string.go
+				type stringStruct struct {
+					str unsafe.Pointer     // str *int   8 byte
+					len int
+				}
+
+				type stringStructDWARF struct {
+					str *byte				// str *uint8  1byte
+					len int
+				}
+
+				int  	字节大小:8  最小值:-9223372036854775808  最大值:9223372036854775807  2^(64-1)-1:  	64位有符号整数的数据范围: -9223372036854775808 到 9223372036854775807
+				uint8  	字节大小:1  最小值:0  					  最大值:255  				 2^8:  			8位无符号整数的数据范围: 0 到 255
+
+				编码 UTF-8，无 NULL 结尾，默认值 ""。
+				使用 `raw string` 定义原始字符串。
+				支持 !=、==、<、<=、>=、>、+、+=。
+				索引访问字节数组（非字符），不能获取元素地址。
+				切片返回子串，依旧指向原数组。
+				内置函数 len 返回字节数组长度。
+
+		-
+	*/
+
 	{
 		/*
 			"雨痕"：这是一个包含中文字符的普通字符串，没有使用任何转义。
@@ -566,16 +567,17 @@ func TestString(t *testing.T) {
 
 }
 
-/*
-array:
-
-	+---+---+---+----//---+----+
-	| 0 | 1 | 2 | ... ... | 99 |   [100]int
-	+---+---+---+----//---+----+
-
--
-*/
 func TestArray(t *testing.T) {
+	/*
+		array:
+
+			+---+---+---+----//---+----+
+			| 0 | 1 | 2 | ... ... | 99 |   [100]int
+			+---+---+---+----//---+----+
+
+		-
+	*/
+
 	{
 		var d1 [2]int // 编译期计算。
 		var d2 [2]int // 元素类型相同，长度不同！
@@ -674,37 +676,38 @@ func TestArray(t *testing.T) {
 
 }
 
-/*
-slice:
-
-		  +---------+            +---+---+----//---+----+
-		  |  array -|----------> | 0 | 1 | ... ... | 99 |
-		  +---------+            +---+---+----//---+----+
-		  |  len    |
-		  +---------+            array
-		  |  cap    |
-		  +---------+
-	     	header
-
--
-
-		// runtime/slice.go
-		type slice struct {
-			array unsafe.Pointer
-			len   int
-			cap   int
-		}
-
-	s := a[2:6:8]
-	+---+---+---+---+---+---+---+---+---+---+
-	| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |   array
-	+---+---+---+---+---+---+---+---+---+---+
-			|               |       |           slice: [low : high : max]
-			|<--- s.len --->|       |
-			|                       |           len = high - low
-			|<------- s.cap ------->|           cap = max  - low
-*/
 func TestSlice(t *testing.T) {
+	/*
+		slice:
+
+				  +---------+            +---+---+----//---+----+
+				  |  array -|----------> | 0 | 1 | ... ... | 99 |
+				  +---------+            +---+---+----//---+----+
+				  |  len    |
+				  +---------+            array
+				  |  cap    |
+				  +---------+
+			     	header
+
+		-
+
+				// runtime/slice.go
+				type slice struct {
+					array unsafe.Pointer
+					len   int
+					cap   int
+				}
+
+			s := a[2:6:8]
+			+---+---+---+---+---+---+---+---+---+---+
+			| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |   array
+			+---+---+---+---+---+---+---+---+---+---+
+					|               |       |           slice: [low : high : max]
+					|<--- s.len --->|       |
+					|                       |           len = high - low
+					|<------- s.cap ------->|           cap = max  - low
+	*/
+
 	a := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9} // array
 
 	s := a[2:6:8]
@@ -743,397 +746,68 @@ func TestSlice(t *testing.T) {
 
 }
 
-// ### 映射类型
-func TestSwitch8(t *testing.T) {}
+// 映射类型 字典
+func TestMap(t *testing.T) {}
 
-// ```
-// var m map[string]int
-// m = make(map[string]int)
-// m["key"] = 42
-//
-// // 删除某个键
-// delete(m, "key")
-//
-// // 测试该键对应的值是否存在
-// elem, has_value := m["key"]
-//
-// // map literal
-//
-//	var m = map[string]Vertex{
-//	   "Bell Labs": {40.68433, -74.39967},
-//	   "Google":    {37.42202, -122.08408},
-//	}
-//
-// ```
-//
-// ## Struct & Interface: 结构体与接口
-//
-// ### Struct: 结构体
-func TestSwitch9(t *testing.T) {}
+// 结构体
+func TestStruct(t *testing.T) {}
 
-// Go 语言中并不存在类的概念，只有结构体，结构体可以看做属性的集合，同时可以为其定义方法。
-//
-// ```
-// // 声明结构体
-//
-//	type Vertex struct {
-//	   // 结构体的属性，同样遵循大写导出，小写私有的原则
-//	   X, Y int
-//	   z bool
-//	}
-//
-// // 也可以声明隐式结构体
-//
-//	point := struct {
-//	   X, Y int
-//	}{1, 2}
-//
-// // 创建结构体实例
-// var v = Vertex{1, 2}
-//
-// // 读取或者设置属性
-// v.X = 4;
-//
-// // 显示声明键
-// var v = Vertex{X: 1, Y: 2}
-//
-// // 声明数组
-// var v = []Vertex{{1,2},{5,2},{5,5}}
-// ```
-//
-// 方法的声明也非常简洁，只需要在 func 关键字与函数名之间声明结构体指针即可，该结构体会在不同的方法间进行复制：
-//
-// ```
-//
-//	func (v Vertex) Abs() float64 {
-//	   return math.Sqrt(v.X*v.X + v.Y*v.Y)
-//	}
-//
-// // Call method
-// v.Abs()
-// ```
-//
-// 对于那些需要修改当前结构体对象的方法，则需要传入指针：
-//
-// ```
-//
-//	func (v *Vertex) add(n float64) {
-//	   v.X += n
-//	   v.Y += n
-//	}
-//
-// ```
-//
-// ```
-// var p *Person = new(Person) // pointer of type Person
-// ```
-//
-// ### Pointer: 指针
-func TestSwitch10(t *testing.T) {}
+// 指针
+func TestPointer(t *testing.T) {}
 
-// ```
-// // p 是 Vertex 类型
-// p := Vertex{1, 2}
-//
-// // q 是指向 Vertex 的指针
-// q := &p
-//
-// // r 同样是指向 Vertex 对象的指针
-// r := &Vertex{1, 2}
-//
-// // 指向 Vertex 结构体对象的指针类型为 *Vertex
-// var s *Vertex = new(Vertex)
-// ```
-//
-// ### Interface: 接口
-func TestSwitch11(t *testing.T) {}
+/*
+	======================		method		======================
 
-//
-//Go 允许我们通过定义接口的方式来实现多态性：
-//
-//```
-//// 接口声明
-//type Awesomizer interface {
-//    Awesomize() string
-//}
-//
-//// 结构体并不需要显式实现接口
-//type Foo struct {}
-//
-//// 而是通过实现所有接口规定的方法的方式，来实现接口
-//func (foo Foo) Awesomize() string {
-//    return "Awesome!"
-//}
-//```
-//
-//```
-//type Shape interface {
-//   area() float64
-//}
-//
-//func getArea(shape Shape) float64 {
-//   return shape.area()
-//}
-//
-//type Circle struct {
-//   x,y,radius float64
-//}
-//
-//type Rectangle struct {
-//   width, height float64
-//}
-//
-//func(circle Circle) area() float64 {
-//   return math.Pi * circle.radius * circle.radius
-//}
-//
-//func(rect Rectangle) area() float64 {
-//   return rect.width * rect.height
-//}
-//
-//func main() {
-//   circle := Circle{x:0,y:0,radius:5}
-//   rectangle := Rectangle {width:10, height:5}
-//
-//   fmt.Printf("Circle area: %f\n",getArea(circle))
-//   fmt.Printf("Rectangle area: %f\n",getArea(rectangle))
-//}
-////Circle area: 78.539816
-////Rectangle area: 50.000000
-//```
-//
-//惯用的思路是先定义接口，再定义实现，最后定义使用的方法：
-//
-//```
-//package animals
-//
-//type Animal interface {
-//    Speaks() string
-//}
-//
-//// implementation of Animal
-//type Dog struct{}
-//func (a Dog) Speaks() string { return "woof" }
-//
-///** 在需要的地方直接引用 **/
-//
-//package circus
-//
-//import "animals"
-//
-//func Perform(a animal.Animal) { return a.Speaks() }
-//```
-//
-//Go 也为我们提供了另一种接口的实现方案，我们可以不在具体的实现处定义接口，而是在需要用到该接口的地方，该模式为：
-//
-//```
-//func funcName(a INTERFACETYPE) CONCRETETYPE
-//```
-//
-//定义接口：
-//
-//```
-//package animals
-//
-//type Dog struct{}
-//func (a Dog) Speaks() string { return "woof" }
-//
-///** 在需要使用实现的地方定义接口 **/
-//package circus
-//
-//type Speaker interface {
-//    Speaks() string
-//}
-//
-//func Perform(a Speaker) { return a.Speaks() }
-//```
-//
+-
+*/
+func TestMethod(t *testing.T) {}
 
-// ### Embedding
-func TestSwitch12(t *testing.T) {}
+/*
+	======================		interface		======================
 
-// Go 语言中并没有子类继承这样的概念，而是通过嵌入（Embedding）的方式来
-// 实现类或者接口的组合。
-//
-// ```
-// // ReadWriter 的实现需要同时满足 Reader 与 Writer
-//
-//	type ReadWriter interface {
-//	   Reader
-//	   Writer
-//	}
-//
-// // Server 暴露了所有 Logger 结构体的方法
-//
-//	type Server struct {
-//	   Host string
-//	   Port int
-//	   *log.Logger
-//	}
-//
-// // 初始化方式并未受影响
-// server := &Server{"localhost", 80, log.New(...)}
-//
-// // 却可以直接调用内嵌结构体的方法，等价于 server.Logger.Log(...)
-// server.Log(...)
-//
-// // 内嵌结构体的名词即是类型名
-// var logger *log.Logger = server.Logger
-// ```
-//
-// ## 并发编程
-func TestSwitch13(t *testing.T) {}
+-
+*/
+func TestInterface(t *testing.T) {}
 
-// ### Goroutines
-//
-// Goroutines 是轻量级的线程，可以参考[并发编程导论](https://parg.co/UnK)一文中的进程、线程与协程的讨论；Go 为我们提供了非常便捷的 Goroutines 语法：
-//
-// ```
-// // 普通函数
-// func doStuff(s string) {
-// }
-//
-//	func main() {
-//	   // 使用命名函数创建 Goroutine
-//	   go doStuff("foobar")
-//
-//	   // 使用匿名内部函数创建 Goroutine
-//	   go func (x int) {
-//	       // function body goes here
-//	   }(42)
-//	}
-//
-// ```
-//
-// ### Channels
-func TestSwitch14(t *testing.T) {}
+/*
+	======================		generic		======================
 
-// 信道（Channel）是带有类型的管道，可以用于在不同的 Goroutine 之间传递消息，其基础操作如下：
-//
-// ```
-// // 创建类型为 int 的信道
-// ch := make(chan int)
-//
-// // 向信道中发送值
-// ch <- 42
-//
-// // 从信道中获取值
-// v := <-ch
-//
-// // 读取，并且判断其是否关闭
-// v, ok := <-ch
-//
-// // 读取信道，直至其关闭
-//
-//	for i := range ch {
-//	   fmt.Println(i)
-//	}
-//
-// ```
-//
-// 譬如我们可以在主线程中等待来自 Goroutine 的消息，并且输出：
-//
-// ```
-// // 创建信道
-// messages := make(chan string)
-//
-// // 执行 Goroutine
-// go func() { messages <- "ping" }()
-//
-// // 阻塞，并且等待消息
-// msg := <-messages
-//
-// // 使用信道进行并发地计算，并且阻塞等待结果
-// c := make(chan int)
-// go sum(s[:len(s)/2], c)
-// go sum(s[len(s)/2:], c)
-// x, y := <-c, <-c // 从 c 中接收
-// ```
-//
-// 如上创建的是无缓冲型信道（Non-buffered Channels），其是阻塞型信道；当没有值时读取方会持续阻塞，而写入方则是在无读取时阻塞。我们可以创建缓冲型信道（Buffered Channel），其读取方在信道被写满前都不会被阻塞：
-//
-// ```
-// ch := make(chan int, 100)
-//
-// // 发送方也可以主动关闭信道
-// close(ch)
-// ```
-//
-// Channel 同样可以作为函数参数，并且我们可以显式声明其是用于发送信息还是接收信息，从而增加程序的类型安全度：
-//
-// ```
-// // ping 函数用于发送信息
-//
-//	func ping(pings chan<- string, msg string) {
-//	   pings <- msg
-//	}
-//
-// // pong 函数用于从某个信道中接收信息，然后发送到另一个信道中
-//
-//	func pong(pings <-chan string, pongs chan<- string) {
-//	   msg := <-pings
-//	   pongs <- msg
-//	}
-//
-//	func main() {
-//	   pings := make(chan string, 1)
-//	   pongs := make(chan string, 1)
-//	   ping(pings, "passed message")
-//	   pong(pings, pongs)
-//	   fmt.Println(<-pongs)
-//	}
-//
-// ```
-//
-// ### 同步
-func TestSwitch15(t *testing.T) {}
+-
+*/
+func TestGeneric(t *testing.T) {}
 
-//同步，是并发编程中的常见需求，这里我们可以使用 Channel 的阻塞特性来实现 Goroutine 之间的同步：
-//
-//```
-//func worker(done chan bool) {
-//    time.Sleep(time.Second)
-//    done <- true
-//}
-//
-//func main() {
-//    done := make(chan bool, 1)
-//    go worker(done)
-//
-//    // 阻塞直到接收到消息
-//    <-done
-//}
-//```
-//
-//Go 还为我们提供了 select 关键字，用于等待多个信道的执行结果：
-//
-//```
-//// 创建两个信道
-//c1 := make(chan string)
-//c2 := make(chan string)
-//
-//// 每个信道会以不同时延输出不同值
-//go func() {
-//    time.Sleep(1 * time.Second)
-//    c1 <- "one"
-//}()
-//go func() {
-//    time.Sleep(2 * time.Second)
-//    c2 <- "two"
-//}()
-//
-//// 使用 select 来同时等待两个信道的执行结果
-//for i := 0; i < 2; i++ {
-//    select {
-//    case msg1 := <-c1:
-//        fmt.Println("received", msg1)
-//    case msg2 := <-c2:
-//        fmt.Println("received", msg2)
-//    }
-//}
-//```
-//
+/*
+	======================		concurrency		======================
+	goroutine
+	channel
+	sync
 
+-
+*/
+func TestConcurrency(t *testing.T) {}
+
+/*
+	======================		package		======================
+
+-
+*/
+func TestPackage(t *testing.T) {}
+
+/*
+	======================		ASM/CGO		======================
+
+-
+*/
+func TestASM(t *testing.T) {}
+func TestCGO(t *testing.T) {}
+
+/*
+	======================		testing		======================
+
+-
+*/
 // unit testing
+func TestUintTesting(t *testing.T) {}
 
 // bench testing
+func TestBenchTesting(t *testing.T) {}
